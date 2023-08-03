fc2f7006159676de02d174743f257c432290ab44
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2023-08-04 02:45:16 +0900

    Implement: show rewarded video

--- MaioSample/MaioSample/ViewController.swift
+++ MaioSample/MaioSample/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import Maio
+
+let zoneIdForRewarded = "DemoPublisherZone"
 
 class ViewController: UIViewController {
+
+    var rewarded: MaioRewarded?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func loadRewarded(_ sender: Any) {
+        let request = MaioRequest(zoneId: zoneIdForRewarded, testMode: true)
+        rewarded = MaioRewarded.loadAd(request: request, callback: nil)
+    }
 
+    @IBAction func showRewarded(_ sender: Any) {
+        rewarded?.show(viewContext: self, callback: nil)
+    }
 }
 

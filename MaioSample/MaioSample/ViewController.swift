53b141495322278008c8cc5ea604d3ce06a65a5d
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2023-08-03 23:52:45 +0900

    Add project: Sample app

---
+++ MaioSample/MaioSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MaioSample
+//
+//  Created by im-ttmskk on 2023/08/03.
+//  Copyright © 2023 Maio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

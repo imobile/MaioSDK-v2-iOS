0eb6ee52a5700b3471a5601b5f336e9a5dd20422
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2023-08-04 00:37:41 +0900

    Add Maio.xcframework

---
+++ Maio.xcframework/ios-arm64/Maio.framework/Headers/Maio.h
@@ -0,0 +1,33 @@
+//
+//  Maio.h
+//  Maio
+//
+//  Created by Kasamatsu on 2019/08/27.
+//  Copyright © 2019 Maio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <Maio/PublicHeader.h>
+
+#import "ZipArchive.h"
+#import "minishared.h"
+#import "ioapi_mem.h"
+#import "crypt.h"
+#import "zip.h"
+#import "unzip.h"
+#import "ioapi.h"
+#import "ioapi_buf.h"
+#import "prng.h"
+#import "aes_ni.h"
+#import "aestab.h"
+#import "pwd2key.h"
+#import "aesopt.h"
+#import "brg_types.h"
+#import "hmac.h"
+#import "brg_endian.h"
+#import "aes.h"
+#import "sha1.h"
+#import "fileenc.h"
+#import "SSZipArchive.h"
+#import "SSZipCommon.h"
---
+++ Maio.xcframework/ios-arm64_x86_64-simulator/Maio.framework/Headers/NamespacedDependencies.h
@@ -0,0 +1,628 @@
+//
+//  Header.h
+//  Maio
+//
+//  Created by Kasamatsu on 2018/05/31.
+//
+
+#ifndef NamespacedDependencies_h
+#define NamespacedDependencies_h
+
+// Constraints
+
+#ifndef SSZipArchiveErrorDomain
+#define SSZipArchiveErrorDomain MaioDeps_SSZipArchiveErrorDomain
+#endif
+
+
+// Classes
+
+#ifndef SSZipArchive
+#define SSZipArchive MaioDeps_SSZipArchive
+#endif
+
+
+// Functions
+
+#ifndef _fileIsSymbolicLink
+#define _fileIsSymbolicLink MaioDeps__fileIsSymbolicLink
+#endif
+
+#ifndef _zipOpenEntry
+#define _zipOpenEntry MaioDeps__zipOpenEntry
+#endif
+
+#ifndef aes_decrypt
+#define aes_decrypt MaioDeps_aes_decrypt
+#endif
+
+#ifndef aes_decrypt_key
+#define aes_decrypt_key MaioDeps_aes_decrypt_key
+#endif
+
+#ifndef aes_decrypt_key128
+#define aes_decrypt_key128 MaioDeps_aes_decrypt_key128
+#endif
+
+#ifndef aes_decrypt_key192
+#define aes_decrypt_key192 MaioDeps_aes_decrypt_key192
+#endif
+
+#ifndef aes_decrypt_key256
+#define aes_decrypt_key256 MaioDeps_aes_decrypt_key256
+#endif
+
+#ifndef aes_encrypt
+#define aes_encrypt MaioDeps_aes_encrypt
+#endif
+
+#ifndef aes_encrypt_key
+#define aes_encrypt_key MaioDeps_aes_encrypt_key
+#endif
+
+#ifndef aes_encrypt_key128
+#define aes_encrypt_key128 MaioDeps_aes_encrypt_key128
+#endif
+
+#ifndef aes_encrypt_key192
+#define aes_encrypt_key192 MaioDeps_aes_encrypt_key192
+#endif
+
+#ifndef aes_encrypt_key256
+#define aes_encrypt_key256 MaioDeps_aes_encrypt_key256
+#endif
+
+#ifndef aes_init
+#define aes_init MaioDeps_aes_init
+#endif
+
+#ifndef call_zopen64
+#define call_zopen64 MaioDeps_call_zopen64
+#endif
+
+#ifndef call_zopendisk64
+#define call_zopendisk64 MaioDeps_call_zopendisk64
+#endif
+
+#ifndef call_zseek64
+#define call_zseek64 MaioDeps_call_zseek64
+#endif
+
+#ifndef call_ztell64
+#define call_ztell64 MaioDeps_call_ztell64
+#endif
+
+#ifndef change_file_date
+#define change_file_date MaioDeps_change_file_date
+#endif
+
+#ifndef check_file_exists
+#define check_file_exists MaioDeps_check_file_exists
+#endif
+
+#ifndef crypthead
+#define crypthead MaioDeps_crypthead
+#endif
+
+#ifndef cryptrand
+#define cryptrand MaioDeps_cryptrand
+#endif
+
+#ifndef decrypt_byte
+#define decrypt_byte MaioDeps_decrypt_byte
+#endif
+
+#ifndef derive_key
+#define derive_key MaioDeps_derive_key
+#endif
+
+#ifndef display_zpos64
+#define display_zpos64 MaioDeps_display_zpos64
+#endif
+
+#ifndef dosdate_to_raw_tm
+#define dosdate_to_raw_tm MaioDeps_dosdate_to_raw_tm
+#endif
+
+#ifndef dosdate_to_time_t
+#define dosdate_to_time_t MaioDeps_dosdate_to_time_t
+#endif
+
+#ifndef dosdate_to_tm
+#define dosdate_to_tm MaioDeps_dosdate_to_tm
+#endif
+
+#ifndef fclose_buf_func
+#define fclose_buf_func MaioDeps_fclose_buf_func
+#endif
+
+#ifndef fclose_mem_func
+#define fclose_mem_func MaioDeps_fclose_mem_func
+#endif
+
+#ifndef fcrypt_decrypt
+#define fcrypt_decrypt MaioDeps_fcrypt_decrypt
+#endif
+
+#ifndef fcrypt_encrypt
+#define fcrypt_encrypt MaioDeps_fcrypt_encrypt
+#endif
+
+#ifndef fcrypt_end
+#define fcrypt_end MaioDeps_fcrypt_end
+#endif
+
+#ifndef fcrypt_init
+#define fcrypt_init MaioDeps_fcrypt_init
+#endif
+
+#ifndef ferror_buf_func
+#define ferror_buf_func MaioDeps_ferror_buf_func
+#endif
+
+#ifndef ferror_mem_func
+#define ferror_mem_func MaioDeps_ferror_mem_func
+#endif
+
+#ifndef fflush_buf
+#define fflush_buf MaioDeps_fflush_buf
+#endif
+
+#ifndef fill_buffer_filefunc
+#define fill_buffer_filefunc MaioDeps_fill_buffer_filefunc
+#endif
+
+#ifndef fill_buffer_filefunc64
+#define fill_buffer_filefunc64 MaioDeps_fill_buffer_filefunc64
+#endif
+
+#ifndef fill_fopen64_filefunc
+#define fill_fopen64_filefunc MaioDeps_fill_fopen64_filefunc
+#endif
+
+#ifndef fill_fopen_filefunc
+#define fill_fopen_filefunc MaioDeps_fill_fopen_filefunc
+#endif
+
+#ifndef fill_memory_filefunc
+#define fill_memory_filefunc MaioDeps_fill_memory_filefunc
+#endif
+
+#ifndef fill_zlib_filefunc64_32_def_from_filefunc32
+#define fill_zlib_filefunc64_32_def_from_filefunc32 MaioDeps_fill_zlib_filefunc64_32_def_from_filefunc32
+#endif
+
+#ifndef fopen64_buf_func
+#define fopen64_buf_func MaioDeps_fopen64_buf_func
+#endif
+
+#ifndef fopen_buf_func
+#define fopen_buf_func MaioDeps_fopen_buf_func
+#endif
+
+#ifndef fopen_buf_internal_func
+#define fopen_buf_internal_func MaioDeps_fopen_buf_internal_func
+#endif
+
+#ifndef fopen_mem_func
+#define fopen_mem_func MaioDeps_fopen_mem_func
+#endif
+
+#ifndef fopendisk64_buf_func
+#define fopendisk64_buf_func MaioDeps_fopendisk64_buf_func
+#endif
+
+#ifndef fopendisk_buf_func
+#define fopendisk_buf_func MaioDeps_fopendisk_buf_func
+#endif
+
+#ifndef fopendisk_mem_func
+#define fopendisk_mem_func MaioDeps_fopendisk_mem_func
+#endif
+
+#ifndef fread_buf_func
+#define fread_buf_func MaioDeps_fread_buf_func
+#endif
+
+#ifndef fread_mem_func
+#define fread_mem_func MaioDeps_fread_mem_func
+#endif
+
+#ifndef fseek64_buf_func
+#define fseek64_buf_func MaioDeps_fseek64_buf_func
+#endif
+
+#ifndef fseek_buf_func
+#define fseek_buf_func MaioDeps_fseek_buf_func
+#endif
+
+#ifndef fseek_buf_internal_func
+#define fseek_buf_internal_func MaioDeps_fseek_buf_internal_func
+#endif
+
+#ifndef fseek_mem_func
+#define fseek_mem_func MaioDeps_fseek_mem_func
+#endif
+
+#ifndef ftell64_buf_func
+#define ftell64_buf_func MaioDeps_ftell64_buf_func
+#endif
+
+#ifndef ftell_buf_func
+#define ftell_buf_func MaioDeps_ftell_buf_func
+#endif
+
+#ifndef ftell_buf_internal_func
+#define ftell_buf_internal_func MaioDeps_ftell_buf_internal_func
+#endif
+
+#ifndef ftell_mem_func
+#define ftell_mem_func MaioDeps_ftell_mem_func
+#endif
+
+#ifndef fwrite_buf_func
+#define fwrite_buf_func MaioDeps_fwrite_buf_func
+#endif
+
+#ifndef fwrite_mem_func
+#define fwrite_mem_func MaioDeps_fwrite_mem_func
+#endif
+
+#ifndef get_file_date
+#define get_file_date MaioDeps_get_file_date
+#endif
+
+#ifndef get_file_handle
+#define get_file_handle MaioDeps_get_file_handle
+#endif
+
+#ifndef hmac_sha
+#define hmac_sha MaioDeps_hmac_sha
+#endif
+
+#ifndef hmac_sha_begin
+#define hmac_sha_begin MaioDeps_hmac_sha_begin
+#endif
+
+#ifndef hmac_sha_data
+#define hmac_sha_data MaioDeps_hmac_sha_data
+#endif
+
+#ifndef hmac_sha_end
+#define hmac_sha_end MaioDeps_hmac_sha_end
+#endif
+
+#ifndef hmac_sha_key
+#define hmac_sha_key MaioDeps_hmac_sha_key
+#endif
+
+#ifndef init_keys
+#define init_keys MaioDeps_init_keys
+#endif
+
+#ifndef invalid_date
+#define invalid_date MaioDeps_invalid_date
+#endif
+
+#ifndef is_large_file
+#define is_large_file MaioDeps_is_large_file
+#endif
+
+#ifndef makedir
+#define makedir MaioDeps_makedir
+#endif
+
+#ifndef print_buf_internal
+#define print_buf_internal MaioDeps_print_buf_internal
+#endif
+
+#ifndef prng_end
+#define prng_end MaioDeps_prng_end
+#endif
+
+#ifndef prng_init
+#define prng_init MaioDeps_prng_init
+#endif
+
+#ifndef prng_rand
+#define prng_rand MaioDeps_prng_rand
+#endif
+
+#ifndef sha1
+#define sha1 MaioDeps_sha1
+#endif
+
+#ifndef sha1_begin
+#define sha1_begin MaioDeps_sha1_begin
+#endif
+
+#ifndef sha1_compile
+#define sha1_compile MaioDeps_sha1_compile
+#endif
+
+#ifndef sha1_end
+#define sha1_end MaioDeps_sha1_end
+#endif
+
+#ifndef sha1_hash
+#define sha1_hash MaioDeps_sha1_hash
+#endif
+
+#ifndef t_fl
+#define t_fl MaioDeps_t_fl
+#endif
+
+#ifndef t_fn
+#define t_fn MaioDeps_t_fn
+#endif
+
+#ifndef t_il
+#define t_il MaioDeps_t_il
+#endif
+
+#ifndef t_im
+#define t_im MaioDeps_t_im
+#endif
+
+#ifndef t_in
+#define t_in MaioDeps_t_in
+#endif
+
+#ifndef t_rc
+#define t_rc MaioDeps_t_rc
+#endif
+
+#ifndef tm_to_dosdate
+#define tm_to_dosdate MaioDeps_tm_to_dosdate
+#endif
+
+#ifndef unzClose
+#define unzClose MaioDeps_unzClose
+#endif
+
+#ifndef unzCloseCurrentFile
+#define unzCloseCurrentFile MaioDeps_unzCloseCurrentFile
+#endif
+
+#ifndef unzCountEntries
+#define unzCountEntries MaioDeps_unzCountEntries
+#endif
+
+#ifndef unzEndOfFile
+#define unzEndOfFile MaioDeps_unzEndOfFile
+#endif
+
+#ifndef unzGetCurrentFileInfo
+#define unzGetCurrentFileInfo MaioDeps_unzGetCurrentFileInfo
+#endif
+
+#ifndef unzGetCurrentFileInfo64
+#define unzGetCurrentFileInfo64 MaioDeps_unzGetCurrentFileInfo64
+#endif
+
+#ifndef unzGetFilePos
+#define unzGetFilePos MaioDeps_unzGetFilePos
+#endif
+
+#ifndef unzGetFilePos64
+#define unzGetFilePos64 MaioDeps_unzGetFilePos64
+#endif
+
+#ifndef unzGetGlobalComment
+#define unzGetGlobalComment MaioDeps_unzGetGlobalComment
+#endif
+
+#ifndef unzGetGlobalInfo
+#define unzGetGlobalInfo MaioDeps_unzGetGlobalInfo
+#endif
+
+#ifndef unzGetGlobalInfo64
+#define unzGetGlobalInfo64 MaioDeps_unzGetGlobalInfo64
+#endif
+
+#ifndef unzGetLocalExtrafield
+#define unzGetLocalExtrafield MaioDeps_unzGetLocalExtrafield
+#endif
+
+#ifndef unzGetOffset
+#define unzGetOffset MaioDeps_unzGetOffset
+#endif
+
+#ifndef unzGetOffset64
+#define unzGetOffset64 MaioDeps_unzGetOffset64
+#endif
+
+#ifndef unzGoToFilePos
+#define unzGoToFilePos MaioDeps_unzGoToFilePos
+#endif
+
+#ifndef unzGoToFilePos64
+#define unzGoToFilePos64 MaioDeps_unzGoToFilePos64
+#endif
+
+#ifndef unzGoToFirstFile
+#define unzGoToFirstFile MaioDeps_unzGoToFirstFile
+#endif
+
+#ifndef unzGoToFirstFile2
+#define unzGoToFirstFile2 MaioDeps_unzGoToFirstFile2
+#endif
+
+#ifndef unzGoToNextFile
+#define unzGoToNextFile MaioDeps_unzGoToNextFile
+#endif
+
+#ifndef unzGoToNextFile2
+#define unzGoToNextFile2 MaioDeps_unzGoToNextFile2
+#endif
+
+#ifndef unzLocateFile
+#define unzLocateFile MaioDeps_unzLocateFile
+#endif
+
+#ifndef unzOpen
+#define unzOpen MaioDeps_unzOpen
+#endif
+
+#ifndef unzOpen2
+#define unzOpen2 MaioDeps_unzOpen2
+#endif
+
+#ifndef unzOpen2_64
+#define unzOpen2_64 MaioDeps_unzOpen2_64
+#endif
+
+#ifndef unzOpen64
+#define unzOpen64 MaioDeps_unzOpen64
+#endif
+
+#ifndef unzOpenCurrentFile
+#define unzOpenCurrentFile MaioDeps_unzOpenCurrentFile
+#endif
+
+#ifndef unzOpenCurrentFile2
+#define unzOpenCurrentFile2 MaioDeps_unzOpenCurrentFile2
+#endif
+
+#ifndef unzOpenCurrentFile3
+#define unzOpenCurrentFile3 MaioDeps_unzOpenCurrentFile3
+#endif
+
+#ifndef unzOpenCurrentFilePassword
+#define unzOpenCurrentFilePassword MaioDeps_unzOpenCurrentFilePassword
+#endif
+
+#ifndef unzReadCurrentFile
+#define unzReadCurrentFile MaioDeps_unzReadCurrentFile
+#endif
+
+#ifndef unzSeek
+#define unzSeek MaioDeps_unzSeek
+#endif
+
+#ifndef unzSeek64
+#define unzSeek64 MaioDeps_unzSeek64
+#endif
+
+#ifndef unzSetOffset
+#define unzSetOffset MaioDeps_unzSetOffset
+#endif
+
+#ifndef unzSetOffset64
+#define unzSetOffset64 MaioDeps_unzSetOffset64
+#endif
+
+#ifndef unzTell
+#define unzTell MaioDeps_unzTell
+#endif
+
+#ifndef unzTell64
+#define unzTell64 MaioDeps_unzTell64
+#endif
+
+#ifndef unz_copyright
+#define unz_copyright MaioDeps_unz_copyright
+#endif
+
+#ifndef update_keys
+#define update_keys MaioDeps_update_keys
+#endif
+
+#ifndef zipClose
+#define zipClose MaioDeps_zipClose
+#endif
+
+#ifndef zipClose2_64
+#define zipClose2_64 MaioDeps_zipClose2_64
+#endif
+
+#ifndef zipCloseFileInZip
+#define zipCloseFileInZip MaioDeps_zipCloseFileInZip
+#endif
+
+#ifndef zipCloseFileInZipRaw
+#define zipCloseFileInZipRaw MaioDeps_zipCloseFileInZipRaw
+#endif
+
+#ifndef zipCloseFileInZipRaw64
+#define zipCloseFileInZipRaw64 MaioDeps_zipCloseFileInZipRaw64
+#endif
+
+#ifndef zipClose_64
+#define zipClose_64 MaioDeps_zipClose_64
+#endif
+
+#ifndef zipOpen
+#define zipOpen MaioDeps_zipOpen
+#endif
+
+#ifndef zipOpen2
+#define zipOpen2 MaioDeps_zipOpen2
+#endif
+
+#ifndef zipOpen2_64
+#define zipOpen2_64 MaioDeps_zipOpen2_64
+#endif
+
+#ifndef zipOpen3
+#define zipOpen3 MaioDeps_zipOpen3
+#endif
+
+#ifndef zipOpen3_64
+#define zipOpen3_64 MaioDeps_zipOpen3_64
+#endif
+
+#ifndef zipOpen4
+#define zipOpen4 MaioDeps_zipOpen4
+#endif
+
+#ifndef zipOpen64
+#define zipOpen64 MaioDeps_zipOpen64
+#endif
+
+#ifndef zipOpenNewFileInZip
+#define zipOpenNewFileInZip MaioDeps_zipOpenNewFileInZip
+#endif
+
+#ifndef zipOpenNewFileInZip2
+#define zipOpenNewFileInZip2 MaioDeps_zipOpenNewFileInZip2
+#endif
+
+#ifndef zipOpenNewFileInZip2_64
+#define zipOpenNewFileInZip2_64 MaioDeps_zipOpenNewFileInZip2_64
+#endif
+
+#ifndef zipOpenNewFileInZip3
+#define zipOpenNewFileInZip3 MaioDeps_zipOpenNewFileInZip3
+#endif
+
+#ifndef zipOpenNewFileInZip3_64
+#define zipOpenNewFileInZip3_64 MaioDeps_zipOpenNewFileInZip3_64
+#endif
+
+#ifndef zipOpenNewFileInZip4
+#define zipOpenNewFileInZip4 MaioDeps_zipOpenNewFileInZip4
+#endif
+
+#ifndef zipOpenNewFileInZip4_64
+#define zipOpenNewFileInZip4_64 MaioDeps_zipOpenNewFileInZip4_64
+#endif
+
+#ifndef zipOpenNewFileInZip5
+#define zipOpenNewFileInZip5 MaioDeps_zipOpenNewFileInZip5
+#endif
+
+#ifndef zipOpenNewFileInZip64
+#define zipOpenNewFileInZip64 MaioDeps_zipOpenNewFileInZip64
+#endif
+
+#ifndef zipWriteInFileInZip
+#define zipWriteInFileInZip MaioDeps_zipWriteInFileInZip
+#endif
+
+#ifndef zip_copyright
+#define zip_copyright MaioDeps_zip_copyright
+#endif
+
+
+#endif /* Header_h */

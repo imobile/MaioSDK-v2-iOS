9df8ed4b74e0fab5cbf8d8c1eb77859f36618a30
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2023-08-10 03:19:34 +0900

    Edit SampleApp: Separate Rewarded implementation

--- MaioSample/MaioSample/ViewController.swift
+++ MaioSample/MaioSample/ViewController.swift
@@ -13,7 +13,7 @@ let zoneIdForRewarded = "DemoPublisherZone"
 
 class ViewController: UIViewController {
 
-    private var rewarded: MaioRewarded?
+    private let rewarded = MyRewardedAd()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,42 +21,54 @@ class ViewController: UIViewController {
     }
 
     @IBAction func loadRewarded(_ sender: Any) {
+        rewarded.loadAd()
+    }
+
+    @IBAction func showRewarded(_ sender: Any) {
+        rewarded.showAd(viewController: self)
+    }
+
+}
+
+fileprivate func logging(content: String) {
+    let dateformatter = DateFormatter()
+    dateformatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSSSSZ"
+    dateformatter.locale = Locale(identifier: "ja_JP")
+
+    print(dateformatter.string(from: Date()), "[Logging]", "\(content)")
+}
+
+fileprivate class MyRewardedAd {
+    private var rewarded: MaioRewarded?
+
+    func loadAd () {
         let request = MaioRequest(zoneId: zoneIdForRewarded, testMode: true)
         rewarded = MaioRewarded.loadAd(request: request, callback: self)
     }
 
-    @IBAction func showRewarded(_ sender: Any) {
-        rewarded?.show(viewContext: self, callback: self)
-    }
-
-    fileprivate func logging(content: String) {
-        let dateformatter = DateFormatter()
-        dateformatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSSSSZ"
-        dateformatter.locale = Locale(identifier: "ja_JP")
-
-        print(dateformatter.string(from: Date()), "[Logging]", "\(content)")
+    func showAd(viewController: UIViewController) {
+        rewarded?.show(viewContext: viewController, callback: self)
     }
 }
 
-
-extension ViewController: MaioRewardedLoadCallback, MaioRewardedShowCallback {
+extension MyRewardedAd: MaioRewardedLoadCallback, MaioRewardedShowCallback {
     func didLoad(_ ad: MaioRewarded) {
-        self.logging(content: "maio rewarded did load")
+        logging(content: "maio rewarded did load")
     }
 
     func didFail(_ ad: MaioRewarded, errorCode: Int) {
-        self.logging(content: "maio rewarded did fail: errorcode: \(errorCode)")
+        logging(content: "maio rewarded did fail: errorcode: \(errorCode)")
     }
 
     func didOpen(_ ad: MaioRewarded) {
-        self.logging(content: "maio rewarded did open")
+        logging(content: "maio rewarded did open")
     }
 
     func didClose(_ ad: MaioRewarded) {
-        self.logging(content: "maio rewarded did close")
+        logging(content: "maio rewarded did close")
     }
 
     func didReward(_ ad: MaioRewarded, reward: RewardData) {
-        self.logging(content: "maio rewarded did reward: \(reward.value)")
+        logging(content: "maio rewarded did reward: \(reward.value)")
     }
 }
